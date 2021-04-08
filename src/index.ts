18a91ac9890d9f3f5dfb29a9b05fbff37e915622
Eugene <oleynik.zh10@gmail.com>
2021-04-09 02:07:18 +0300

    Pack and Compile scenes added to bot

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import { Telegraf } from 'telegraf'
+import { Scenes, session, Telegraf } from 'telegraf'
 import { BOT_TOKEN } from './config/bot.config'
 import { try_eval_math_expr } from './functions/evaluate_expr/eval_expr'
-import {DEFAULT_STICKER_ID} from './globals/globals'
+import { DEFAULT_STICKER_ID } from './globals/globals'
+import { pack_scene } from './controllers/pack'
+import { compile_scene } from './controllers/compile_cpp'
 
 const bot = new Telegraf(BOT_TOKEN)
+
+const stage = new Scenes.Stage([pack_scene, compile_scene])
+
+bot.use(session())
+bot.use(stage.middleware())
 
 bot.start((ctx) => {
     ctx.reply("HELLO!")
@@ -11,6 +18,18 @@ bot.start((ctx) => {
 
 bot.on('sticker', (ctx) => {
     ctx.replyWithSticker(DEFAULT_STICKER_ID)
+})
+
+// handle commands
+bot.command('pack', (ctx) => {
+    console.log(ctx)
+    // @ts-ignore
+    ctx.scene.enter('__PACK_SCENE__')
+})
+
+bot.command('compile', (ctx) => {
+    // @ts-ignore
+    ctx.scene.enter('__COMPILE_SCENE__')
 })
 
 bot.on('message', (ctx) => {
@@ -24,6 +43,7 @@ bot.on('message', (ctx) => {
     else
         ctx.replyWithSticker(DEFAULT_STICKER_ID)
 })
+
 
 bot.launch().then(() => console.log('Bot launched...'))
     .catch(err => console.log("Error occurred launching bot: ", err))
